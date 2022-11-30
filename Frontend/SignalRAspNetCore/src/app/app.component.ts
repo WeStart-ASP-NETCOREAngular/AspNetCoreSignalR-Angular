28e5ec43d5392cab5b95220f57bdef45dae8a82c
Wasim Fatair <wasim.fatair@gmail.com>
2022-11-30 10:08:58 +0200

    Angular with @microsoft/signalr npm installed make sure to exec npm install

---
+++ Frontend/SignalRAspNetCore/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as signalR from '@microsoft/signalr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'SignalRAspNetCore';
+  messages: string[] = [];
+  chatForm: FormGroup;
+  users: string[];
+  hubConnection: signalR.HubConnection;
+
+  ngOnInit(): void {
+    this.chatForm = new FormGroup({
+      message: new FormControl(''),
+      group: new FormControl('Everyone'),
+    });
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7167/Messages')
+      .build();
+
+    this.hubConnection.on('ReceiveMessage', (msg) => {
+      console.log(msg);
+      this.messages.push(msg);
+    });
+
+    this.hubConnection.on('UsersConnected', (users) => {
+      console.log(users);
+      this.users = users;
+    });
+
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('connection Started ...');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  onSubmit() {
+    const { message, group } = this.chatForm.value;
+    if (group == 'Everyone') {
+      this.hubConnection.invoke('SendMessageToAll', message);
+    } else {
+      this.hubConnection.invoke('SendMessageToUser', group, message);
+    }
+
+    console.log(message);
+  }
+}
